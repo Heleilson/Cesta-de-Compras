f1b2edc1af55760de7082072310632381d6b6f72
Leandro Prata <heleilsonpena@gmail.com>
2018-05-30 20:28:36 -0300

    primeiro commit

---
+++ www/js/routes.js
@@ -0,0 +1,23 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+    
+  {
+    path: '/lista/',
+    url: './paginas/lista.html',
+  },
+    
+  {
+    path: '/produto/',
+    url: './paginas/produto.html',
+  },
+    
+     {
+    path: '/sobre/',
+    url: './paginas/sobre.html',
+  },
+ 
+  
+];
